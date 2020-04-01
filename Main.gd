2c24e47bd5e82e91e95373c15d4186525db1a9de
Daichi Murokami <daichi@murokami.com>
2020-03-31 19:26:51 -0700

    Minor update

--- Main.gd
+++ Main.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	# launch test scene
 	get_tree().change_scene("res://Scenes/TestScene.tscn")
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
