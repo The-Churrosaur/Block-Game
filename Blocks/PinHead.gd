cce4c8c6782568ff3db9ecb4b1a44be6e834027f
Daichi Murokami <daichi@murokami.com>
2021-03-11 02:44:16 -0800

    last commit for now, moving to separate hex project. Added entry point in block for block collision detection by tilemap hitbox, and post-load block setup

--- Blocks/PinHead.gd
+++ Blocks/PinHead.gd
@@ -1,15 +1,2 @@
-class_name PinHead
-extends Block
+extends PinHeadBase
 
-# notifies pinblock
-signal pin_grid_changed(pinHead)
-
-func on_added_to_grid(center_coord, block, grid):
-	.on_added_to_grid(center_coord, block, grid)
-	
-	# listen for grid changes/ship movement
-	grid.connect("block_added", self, "on_grid_changed")
-	grid.connect("block_removed", self, "on_grid_changed")
-
-func on_grid_changed(coord, block, grid):
-	emit_signal("pin_grid_changed", self)
--- Blocks/TestBlock.gd
+++ Blocks/TestBlock.gd
@@ -4,6 +4,7 @@ extends Block
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var selfblock
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +15,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _input(event):
+	# for testing purposes
+#	if event.is_action_pressed("ui_accept"):
+#		print(selfblock)
+	
+	pass
+
+func ship_body_entered(body, pos):
+	.ship_body_entered(body, pos)
+	print("block hit: ", self)
+	$Sprite.visible = false
+
+func post_load_setup():
+	.post_load_setup()
+	# test load times
+	# result is negligible
+#	selfblock = grid.get_block(center_grid_coord)
+#	print(selfblock)
+	pass
